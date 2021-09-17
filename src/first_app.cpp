7e6ec4c17b4152540904ca81953d80ac887d8354
austin.moore <austin.moore@resonantsciences.com>
2021-09-17 19:56:34 -0400

    Added code for stats
    This generates a 40x40x40 cube of vases.
    It takes on average 1.2 seconds to generate a frame with this many vases and no culling.

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -15,6 +15,7 @@
 #include <cassert>
 #include <chrono>
 #include <stdexcept>
+#include <iostream>
 
 namespace lve {
 
@@ -47,12 +48,11 @@ namespace lve {
       float frameTime =
         std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
       currentTime = newTime;
-
       cameraController.moveInPlaneXZ(lveWindow.getGLFWwindow(), frameTime, viewerObject);
       camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
-
+      std::cout << frameTime << "\n";
       float aspect = lveRenderer.getAspectRatio();
-      camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
+      camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 100.f);
 
       if (auto commandBuffer = lveRenderer.beginFrame()) {
         lveRenderer.beginSwapChainRenderPass(commandBuffer);
@@ -71,10 +71,58 @@ namespace lve {
   void FirstApp::loadGameObjects() {
     std::shared_ptr<LveModel> lveModel = LveModel::createModelFromFile(lveDevice, "../models/flat_vase.obj");
 
-    auto gameObj = LveGameObject::createGameObject();
-    gameObj.model = lveModel;
-    gameObj.transform.translation = {.0f, .5f, 2.5f};
-    gameObj.transform.scale = glm::vec3(3.f);
-    gameObjects.push_back(std::move(gameObj));
+    //generate vec
+    auto gen = [](glm::vec3 offset, glm::vec3 start, std::size_t count) {
+      std::vector<glm::vec3> pos;
+      glm::vec3 offseto;
+      offseto[0] = offset[0];
+      offseto[1] = offset[1];
+      offseto[2] = offset[2];
+      for(int i = 0; i < count; i++) {
+        offset[0] = (float)i * offseto[0] + offseto[0];
+        offset[1] = (float)i * offseto[1] + offseto[1];
+        offset[2] = (float)i * offseto[2] + offseto[2];
+        pos.push_back(offset + start);
+      }
+      return pos;
+    };
+    auto genRow = [&gen](glm::vec3 start, std::size_t count) {
+      auto left = gen({1.0f, 0.0f, 0.0f}, start, count);  
+      auto right = gen({1.0f, 0.0f, 0.0f}, start, count);  
+      right.insert(right.end(), left.begin(), left.end());
+      return right;
+    };
+    auto gen2 = [&genRow](auto count, float z) {
+      std::vector<glm::vec3> rows;  
+      for(int i = 0; i <= count/2; i++) {
+        auto row = genRow({1.0f, float(i), z}, count);
+        rows.insert(rows.end(), row.begin(), row.end());
+      }
+      for(int i = 0; i < count/2; i++) {
+        auto row = genRow({1.0f, -float(i), z}, count);
+        rows.insert(rows.end(), row.begin(), row.end());
+      }
+      return rows;
+    };
+    auto gen3 = [&gen2](auto count) {
+      std::vector<glm::vec3> faces;
+      for(int i = 0; i <= count/2; i++)  {
+        auto face = gen2(count, (2.5*(float)i)+2.5);
+        faces.insert(faces.end(), face.begin(), face.end());
+      }
+      for(int i = 0; i < count/2; i++)  {
+        auto face = gen2(count, (-2.5*(float)i)+2.5);
+        faces.insert(faces.end(), face.begin(), face.end());
+      }
+      return faces;
+    };
+
+    for(auto& vec : gen3(40)) {
+      auto gameObj = LveGameObject::createGameObject();
+      gameObj.model = lveModel;
+      gameObj.transform.translation = vec;
+      gameObj.transform.scale = glm::vec3(3.f);
+      gameObjects.push_back(std::move(gameObj));
+    }    
   }
 }
